c5fc435a960e0574807e22390288677e077fbb1f
bjohnso <bjohnso@student.wethinkcode.co.za>
2019-11-24 23:14:06 +0200

    Improved operation parser

--- computorV1.c
+++ computorV1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 static void	itoa_isnegative(int *n, int *neg)
 {
@@ -36,52 +37,48 @@ static char	*ft_itoa(int n){
 	return (str);
 }
 
-static int      delim(char *str)
+static int      delim(char *str, char **delim)
 {
-        if (*str == '*' || *str == '+' || *str == '-' || *str == '/' || *str == ' ' || *str == '=' || *str == '\n' || *str == '\0')
-                return (1);
-	if(*str == ' ')
-	{
-		str++;
-		if (*str <= '9' && *str >= '0')
-			return(1);
-		str-=2;
-		if (*str == ',' || (*str <= '9' && *str >= '0'))
-			return (1);
-	}
+        int count = 0;
+        while(delim[count]){
+                if (*str == *delim[count] || (*str >= '0' && *str <= '9')){
+                        return (1);
+                }
+                count++;
+        }
         return (0);
 }
 
-static int      count_words(char *str)
+static int      count_words(char *str, char **args)
 {
         int     count;
 
         count = 0;
-        while (*str && delim(str))
+        while (*str && !delim(str, args))
                 str++;
         while (*str)
         {
-                while (*str && delim(str))
+                while (*str && !delim(str, args))
                         str++;
-                if (*str && !delim(str))
+                if (*str && delim(str, args))
                         count++;
-                while (*str && !delim(str))
+                while (*str && delim(str, args))
                         str++;
         }
         return (count);
 }
 
-static char     *malloc_word(char *str)
+static char     *malloc_word(char *str, char **args)
 {
         int     i;
         char    *word;
 
         i = 0;
-        while (str[i] && !delim(str + i))
+        while (str[i] && delim(str + i, args))
                 i++;
         word = (char *)malloc(sizeof(char) * (i + 1));
         i = 0;
-        while (str[i] && !delim(str + i))
+        while (str[i] && delim(str + i, args))
         {
                 word[i] = str[i];
                 i++;
@@ -90,36 +87,99 @@ static char     *malloc_word(char *str)
         return (word);
 }
 
-char            **ft_split(char *str)
+char            **ft_split(char *str, char **args)
 {
         int     i;
         int     words;
         char    **tab;
 
         i = -1;
-        words = count_words(str);
+        words = count_words(str, args);
         tab = (char **)malloc(sizeof(char *) * (words + 1));
         while (*str)
         {
-                while (*str && delim(str))
+                while (*str && !delim(str, args))
                         str++;
-                if (*str && !delim(str))
-                        tab[++i] = malloc_word(str);
-                while (*str && !delim(str))
+                if (*str && delim(str, args))
+                        tab[++i] = malloc_word(str, args);
+                while (*str && delim(str, args))
                         str++;
         }
         tab[++i] = NULL;
         return (tab);
 }
 
-int main(int argc, char **argv){
-    
-    char **terms;
+int            str_len(char *strlen){
+        int count = 0;
 
-    if (argc == 2){
-        terms = ft_split(argv[1]);
-        for (int i = 0; terms[i] != NULL; i++){
-            printf("%s %d %s %s\n", "VALUE ", i+1, ":", terms[i]);
+        while (strlen[count])
+                count++;
+        return count;
+}
+
+char            **half_split(char *str){
+        int     count = 0;
+        char    *tmp = str;
+
+        while (*tmp){
+                if (*tmp == '=')
+                        break;
+                count++;
+                tmp++;
         }
+
+        char *left = (char *)malloc(sizeof (char) * (count + 1));
+        left[count] = '\0';
+
+        for (int i = 0; i < count; i++)
+                left[i] = str[i];
+
+        char *right = (char *)malloc(sizeof (char) * (str_len(str) - count + 1));
+        right[str_len(str) - count] = '\0';
+
+        count = 0;
+        while (*tmp){
+                right[count] = *tmp;
+                count++;
+                tmp++;
+        }
+
+        char **terms = (char **)malloc(sizeof (char *) * 3);
+        terms[2] = NULL;
+
+        terms[0] = left;
+        terms[1] = right;
+
+        return terms;
+}
+
+char            **signs(char **terms){
+}
+
+void            standardize(char *str){
+        char *args[] = {"*", "/" "+", "-", "^", "X", NULL};
+        char **terms = half_split(str);
+        char **left = ft_split(terms[0], args);
+        char **right = ft_split(terms[1], args);
+
+        for (int i = 0; left[i] != NULL; i++){
+            printf("%s %d %s %s\n", "VALUE ", i + 1, ":", left[i]);
+        }
+
+        for (int i = 0; right[i] != NULL; i++){
+            printf("%s %d %s %s\n", "VALUE ", i + 1, ":", right[i]);
+        }
+
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 2){
+        /*terms = ft_split(argv[1]);
+        for (int i = 0; terms[i] != NULL; i++){
+            printf("%s %d %s %s\n", "VALUE ", i + 1, ":", terms[i]);
+        }*/
+
+        standardize(argv[1]);
     }
 }
