47a04a72260689bf92688ab26030d29ae4f9d59d
brandon JOHNSON <bjohnso@c3r1s6.wethinkcode.co.za>
2019-11-25 18:55:00 +0200

    Began structuring quadratic simplifiaction logic

--- computorV1.c
+++ computorV1.c
@@ -9,15 +9,23 @@ void            simplify(char *str){
         free(terms);
 
         HashTable *ht = ht_create(103);
-
+        //TODO: Create own strcat implementation
         //Capture terms inside of hashmap
         for (int i = 0; left_delim[i] != NULL; i++){
                 if (left_delim[i][0] == 'X'){
                         if (i > 1 && left_delim[i - 2][0] >= '0' && left_delim[i - 2][0] <= '9'){
-                                if (ht_get(ht, left_delim[i]))
-                                        ht_put(ht, left_delim[i], left_delim[i - 2]);
-                                else
-                                        ht_put(ht, left_delim[i], left_delim[i - 2]);
+                                if (ht_get(ht, left_delim[i])){
+                                        if (i > 2 && left_delim[i - 3])
+                                                ht_put(ht, left_delim[i], strcat(left_delim[i - 3], left_delim[i - 2]));
+                                        else
+                                                ht_put(ht, left_delim[i], left_delim[i - 2]);
+                                }
+                                else{
+                                        if (i > 2 && left_delim[i - 3])
+                                                ht_put(ht, left_delim[i], strcat(left_delim[i - 3], left_delim[i - 2]));
+                                        else
+                                                ht_put(ht, left_delim[i], left_delim[i - 2]);
+                                }
                         }
                         else{
                                 if (ht_get(ht, left_delim[i]))
@@ -27,14 +35,33 @@ void            simplify(char *str){
                         }
                 }
         }
-
         ht_print(ht);
 
-        /*for (int i = 0; right_delim[i] != NULL; i++){
-            printf("%s %d %s %s\n", "VALUE ", i + 1, ":", right_delim[i]);
-        }*/
+        for (int i = 0; right_delim[i] != NULL; i++){
+                if (right_delim[i][0] == 'X'){
+                        if (i > 1 && right_delim[i - 2][0] >= '0' && right_delim[i - 2][0] <= '9'){
+                                if (ht_get(ht, right_delim[i])){
+                                        if (i > 2 && right_delim[i - 3])
+                                                ht_put(ht, right_delim[i], strcat(right_delim[i - 3], right_delim[i - 2]));
+                                        else
+                                                ht_put(ht, right_delim[i], right_delim[i - 2]);
+                                }
+                                else{
+                                        if (i > 2 && right_delim[i - 3])
+                                                ht_put(ht, right_delim[i], strcat(right_delim[i - 3], right_delim[i - 2]));
+                                        else
+                                                ht_put(ht, right_delim[i], right_delim[i - 2]);
+                                }
+                        }
+                        else{
+                                if (ht_get(ht, right_delim[i]))
+                                        ht_put(ht, right_delim[i], "1");
+                                else
+                                        ht_put(ht, right_delim[i], "1");
+                        }
+                }
+        }
         ht_free(ht);
-
 }
 
 int main(int argc, char **argv)
